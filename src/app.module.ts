fb7ded5a9b9104702be694a55584be8e4b2e5033
Jeshua <hinostrozajeshua@gmail.com>
2022-08-09 12:42:53 -0500

    add: Config module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import configuration from './config/configuration';
+
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot({ load: [configuration] })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+    port: parseInt(process.env.PORT!, 10) || 3000,
+    database: {
+        db: process.env.DB,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT!, 10) || 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        name: process.env.DB_NAME
+    }
+});
