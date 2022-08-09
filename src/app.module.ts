795e63a9a21bbb3646670fe9bd5acc53906c561d
Jeshua <hinostrozajeshua@gmail.com>
2022-08-09 14:26:14 -0500

    update: typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/type-orm-config.service';
 
 @Module({
   imports: [
     UsersModule, 
-    ConfigModule.forRoot({ load: [configuration], envFilePath:[!process.env.NODE_ENV ? '.env' : `enviroments/${process.env.NODE_ENV}.env`] })
+    ConfigModule.forRoot({ load: [configuration], envFilePath:[!process.env.NODE_ENV ? '.env' : `enviroments/${process.env.NODE_ENV}.env`] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
