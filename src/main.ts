8d85c0785fe913d6bd35c0b5c8d6b51aaa5db893
Jeshua <hinostrozajeshua@gmail.com>
2022-08-09 12:58:05 -0500

    update: app get its port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
