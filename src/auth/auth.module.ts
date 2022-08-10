2df5d4816489b7a65fb95ea913ced92066c4589a
Jeshua <hinostrozajeshua@gmail.com>
2022-08-10 10:07:56 -0500

    add: LocalStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
+import { LocalStrategyService } from './service/local-strategy.service';
 
 @Module({
   controllers: [AuthController],
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategyService]
 })
 export class AuthModule {}
---
+++ src/auth/service/local-strategy.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { LoginAuthDto } from '../dto/login-auth-dto';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategyService extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService) { super(); }
+
+    async validate(login: LoginAuthDto) {
+        const userResult = await this.authService.validateUser(login);
+        if (!userResult) throw new UnauthorizedException();
+
+        return userResult;
+    }
+}
