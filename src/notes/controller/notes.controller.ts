02a22a463d96e5d446b20538571f102da19d35d7
Jeshua <hinostrozajeshua@gmail.com>
2022-08-10 16:07:22 -0500

    add: get all notes

--- src/notes/controller/notes.controller.ts
+++ src/notes/controller/notes.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from 'src/auth/decorators/user.decorator';
+import { JwtAuthGuard } from 'src/auth/service/jwt-auth.guard';
+import { User as User_ } from 'src/entities/user.entity';
+import { NotesService } from '../service/notes.service';
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+    constructor(private notesService: NotesService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    getAllNotes(@User() user: User_) {
+        return this.notesService.findAll(user.id);
+    }
+}
