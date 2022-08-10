332636b8cefdcfa45a72e149982ad0500fe325ce
Jeshua <hinostrozajeshua@gmail.com>
2022-08-10 15:50:00 -0500

    add: notes service methods

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from 'src/entities/note.entity';
 import { NotesController } from './controller/notes.controller';
 import { NotesService } from './service/notes.service';
 
 @Module({
   controllers: [NotesController],
   providers: [NotesService],
+  imports: [TypeOrmModule.forFeature([Note])]
 })
 export class NotesModule {}
--- src/notes/service/notes.service.ts
+++ src/notes/service/notes.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Note } from 'src/entities/note.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+    
+    constructor(@InjectRepository(Note) private notesRepo: Repository<Note>) {}
+
+    findAll(userId: number) {
+        return this.notesRepo.findBy({ userId });
+    }
+
+    findOne(id: number, userId: number) {
+        return this.notesRepo.findOneBy({ id, userId });
+    }
+
+    createOne(note: Note) {
+        return this.notesRepo.save(note);
+    }
+
+    async deleteOne(id: number, userId: number) {
+        await this.notesRepo.delete({ id, userId });
+    }
+}
