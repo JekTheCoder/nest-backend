56e8c9ac77501dd981224475942df8be2ac7aee4
Jeshua <hinostrozajeshua@gmail.com>
2022-08-10 12:11:15 -0500

    update: jwt decode validates user in database

--- src/auth/service/jwt.strategy.ts
+++ src/auth/service/jwt.strategy.ts
@@ -1,19 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "src/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private configService: ConfigService) {
+    constructor(private configService: ConfigService, @InjectRepository(User) private userRepo: Repository<User>,) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: configService.getOrThrow('jwt.token')
         })
     }
 
-    async validate(payload: any) {
-        console.log(payload)
-        return { id: payload.sub, username: payload.username };
+    async validate(payload: { sub: number, username: string }) {
+        const user = await this.userRepo.findOneBy({ id: payload.sub, username: payload.username });
+        if (!user) throw new UnauthorizedException();
+
+        return user;
     }
 }
